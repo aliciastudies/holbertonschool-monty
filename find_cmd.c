4c18acc04bb5b0b224c6c03aaa7e7135ccd48a1c
aliciastudies <6722@holbertonstudents.com>
2023-09-05 01:28:48 -0700

    feat: adding funcs

---
+++ find_cmd.c
@@ -0,0 +1,31 @@
+#include "monty.h"
+
+/**
+ * find_cmd - finds and matches input to func
+ * @opcode: pointer to opcode input
+ * @stack: pointer to pointer to topof stack
+ * @line_number: line number of read line
+ * Return: void
+ */
+
+void find_cmd(char *opcode, stack_t **stack, unsigned int line_number)
+{
+	int i;
+	instruction_t instructions[] = {
+		{"push", push},
+		{"pall", pall},
+		{NULL, NULL}
+	};
+	i = 0;
+	while (instructions[i].opcode != NULL)
+	{
+		if (strcmp(opcode, instructions[i].opcode) == 0)
+		{
+			instructions[i].f(stack, line_number);
+			return;
+		}
+		i = i + 1;
+	}
+	fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
+	exit(EXIT_FAILURE);
+}
