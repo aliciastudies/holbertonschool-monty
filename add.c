5e5d023fc5ce67349f3f19bb1c30e595f0138e32
aliciastudies <6722@holbertonstudents.com>
2023-09-05 18:47:42 -0700

    update: added /n

--- add.c
+++ add.c
@@ -11,12 +11,12 @@ void add(stack_t **stack, unsigned int line_number)
 {
 	if (stack == NULL || *stack == NULL)
 	{
-		fprintf(stderr, "L%u: can't add, stack too short", line_number);
+		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	if ((*stack)->next == NULL)
 	{
-		fprintf(stderr, "L%u: can't add, stack too short", line_number);
+		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
 	}
 	(*stack)->next->n = (*stack)->n + (*stack)->next->n;
 	pop(stack, line_number);
