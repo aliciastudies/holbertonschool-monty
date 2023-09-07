51806cc45e46fede572028cd8a35c4c6ed3e674f
aliciastudies <6722@holbertonstudents.com>
2023-09-06 20:57:36 -0700

    update: added exit status

--- add.c
+++ add.c
@@ -17,7 +17,8 @@ void add(stack_t **stack, unsigned int line_number)
 	if ((*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
 	}
-	(*stack)->next->n = (*stack)->n + (*stack)->next->n;
+	(*stack)->next->n = (*stack)->next->n + (*stack)->n;
 	pop(stack, line_number);
 }
